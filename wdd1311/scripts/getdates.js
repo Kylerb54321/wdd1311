272b8c831b250d2ceca304866987db5120b8e16f
Kylerb54321 <bailey.k.r.12@gmail.com>
2025-05-23 13:10:10 +0200

    Delete wdd1311 directory

--- wdd1311/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-const yearSpan = document.getElementById("currentyear");
-yearSpan.textContent = new Date().getFullYear();
-
-const lastModifiedPara = document.getElementById("lastModified");
-lastModifiedPara.textContent = "Last Modified: " + document.lastModified;
